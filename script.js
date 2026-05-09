48b19188109a052e728b6920a6221c4326b3ee97
clonee-Devloper <cloneepy1@gmail.com>
2026-05-09 16:24:23 +0700

    Replace Firebase config placeholders with values
    Updated Firebase configuration with actual credentials.

--- script.js
+++ script.js
@@ -25,20 +25,13 @@ window.onerror = function(message, source, lineno){
 ========================================================= */
 
 const firebaseConfig = {
-
-  apiKey: "YOUR_API_KEY",
-
-  authDomain: "YOUR_AUTH_DOMAIN",
-
-  projectId: "YOUR_PROJECT_ID",
-
-  storageBucket: "YOUR_STORAGE_BUCKET",
-
-  messagingSenderId: "YOUR_SENDER_ID",
-
-  appId: "YOUR_APP_ID"
-
-};
+    apiKey: "AIzaSyD0ADo1TMD5ASjJ_l4062EEJtqZ2irmuIw",
+    authDomain: "boostpanel-myown.firebaseapp.com",
+    projectId: "boostpanel-myown",
+    storageBucket: "boostpanel-myown.firebasestorage.app",
+    messagingSenderId: "1003583759714",
+    appId: "1:1003583759714:web:1b9b88c96825ec5d56783b"
+  };
 
 firebase.initializeApp(firebaseConfig);
 
